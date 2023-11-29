884b7aa3cee6ea0751c8370dad69c0ff8f4e0a2d
snhase <snehal.hase@gmail.com>
2023-11-28 18:28:50 -0800

    Add Countries component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import './App.css';
 import { BsMoon } from "react-icons/bs";
-import { FaSearch } from "react-icons/fa";
+import { Countries } from './assets/components/Countries';
 
 function App() {
 
   const [region, setRegion] = useState("Filter By Region")
-  
+    
   return (
     <div className="d-flex flex-column">
       <header className=" shadow-sm py-3 bg-white">
@@ -19,39 +19,18 @@ function App() {
         </div>
       </header>
       <main className="container mx-auto px-0">
-        <div className="my-4 row justify-content-between align-items-center mx-auto">
-          <div className="col-sm-4">
-            <div className="my-3  bg-white align-items-center rounded-3 shadow-sm">
-              <FaSearch className="mx-3" color="hsl(0, 0%, 52%)" size="1.25rem"/>
-              <input className="mx-3 search-input " placeholder="Search for a country..."></input>
-            </div>
-          </div>
-          <div className="d-flex justify-content-start col-sm-4 justify-content-sm-end">
-            <div className="dropdown">
-              <button 
-                className="btn bg-white dropdown-toggle py-2 px-3" 
-                type="button" 
-                data-bs-toggle="dropdown" 
-                aria-expanded="false">
-                {region}
-              </button>
-                <ul className="dropdown-menu border-0 mt-2 dropdown-menu-end">
-                  {
-                    region != "Filter By Region"?
-                    <div className="dropdown-item" id="FilterByRegion" onClick={()=>{setRegion("Filter By Region")}}>Show All</div>
-                    :
-                    <></>
-                  }
-                <div className="dropdown-item" id="Africa" onClick={()=>{setRegion("Africa")}}>Africa</div>
-                <div className="dropdown-item" id="America" onClick={()=>{setRegion("America")}}>America</div>
-                <div className="dropdown-item" id="Asia" onClick={()=>{setRegion("Asia")}}>Asia</div>
-                <div className="dropdown-item" id="Europe" onClick={()=>{setRegion("Europe")}}>Europe</div>
-                <div className="dropdown-item" id="Oceania" onClick={()=>{setRegion("Oceania")}}>Oceania</div>
-              </ul>
-            </div>
-          </div>
-        </div>
+        <Countries 
+          region={region}
+          setRegion={setRegion}
+          />
       </main>
+      <footer className="p-3 text-center bg-white">
+      Challenge by <a 
+            href="https://www.frontendmentor.io/challenges/rest-countries-api-with-color-theme-switcher-5cacc469fec04111f7b848ca" 
+            alt="frontend-mentor challenge link" 
+            target="_blank" 
+            rel="noopener noreferrer">Frontend Mentor</a>. Coded by <a href="https://github.com/snhase" alt="github url" target="_blank" rel="noopener noreferrer">snhase</a>
+      </footer>
     </div>
   );
 }
---
+++ src/assets/components/Countries.js
@@ -0,0 +1,62 @@
+import { FaSearch } from "react-icons/fa";
+import data from "../data.json"
+
+export const Countries = ({region, setRegion} ) => {
+    return(
+        <div className='mx-auto m-0 p-0'>
+            <div className="my-4 row justify-content-between align-items-center mx-auto">
+          <div className="col-sm-4">
+            <div className="my-3  bg-white align-items-center rounded-3 shadow-sm">
+              <FaSearch className="mx-3" color="hsl(0, 0%, 52%)" size="1.25rem"/>
+              <input className="mx-3 search-input " placeholder="Search for a country..."></input>
+            </div>
+          </div>
+          <div className="d-flex justify-content-start col-sm-4 justify-content-sm-end">
+            <div className="dropdown shadow-sm">
+              <button 
+                className="btn bg-white dropdown-toggle py-2 px-3" 
+                type="button" 
+                data-bs-toggle="dropdown" 
+                aria-expanded="false">
+                {region}
+              </button>
+                <ul className="dropdown-menu border-0 mt-2 dropdown-menu-end">
+                  {
+                    region != "Filter By Region"?
+                    <div className="dropdown-item" id="FilterByRegion" onClick={()=>{setRegion("Filter By Region")}}>Show All</div>
+                    :
+                    <></>
+                  }
+                <div className="dropdown-item" id="Africa" onClick={()=>{setRegion("Africa")}}>Africa</div>
+                <div className="dropdown-item" id="America" onClick={()=>{setRegion("America")}}>America</div>
+                <div className="dropdown-item" id="Asia" onClick={()=>{setRegion("Asia")}}>Asia</div>
+                <div className="dropdown-item" id="Europe" onClick={()=>{setRegion("Europe")}}>Europe</div>
+                <div className="dropdown-item" id="Oceania" onClick={()=>{setRegion("Oceania")}}>Oceania</div>
+              </ul>
+            </div>
+          </div>
+            </div>
+            <div className="row mx-auto">
+              {
+                data.map((country)=>{
+                  return (
+                  <div className="col-md-4 col-lg-3 mb-5">
+                  <div className="card border-light shadow-sm rounded-3">
+                    <img src={country.flags.png} className="card-img-top flag-img" alt="country flag"/>
+                    <div className="card-body p-4 mb-3">
+                      <div className="card-title py-2">{country.name}</div>
+                      <div className="card-text">
+                        <div><b>Population :</b> {(country.population).toLocaleString()}</div>
+                        <div><b>Region :</b> {country.region}</div>
+                        <div><b>Capital :</b> {country.capital}</div>
+                      </div>
+                    </div>
+                  </div>
+                  </div>)
+                })
+              }
+            </div>
+        </div>
+
+    );
+}
